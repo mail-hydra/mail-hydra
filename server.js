52e355cfd288830bbddb7344250d543eeb75c6d6
Christopher <Christopher.Fenner@me.com>
2020-10-07 19:31:56 +0200

    extract code to helper function

--- server.js
+++ server.js
@@ -7,21 +7,27 @@ const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
   client().list().then(function (results) {
-    var json = []
-    results.forEach(element => {
-      json.push({
-        source: element.mail_forward_adress,
-        target: element.mail_forward_targets,
-        comment: element.mail_forward_comment,
-        pending: element.in_progress
-      })
-    })
-    res.json(json)
+    res.json(forwardListToJSON(results))
   })
 })
+
+// start app
 app.listen(port, () => {
   console.log(`Server listening on port ${port}...`)
 })
+
+function forwardListToJSON (forwardList) {
+  var json = []
+  forwardList.forEach(element => {
+    json.push({
+      source: element.mail_forward_adress,
+      target: element.mail_forward_targets,
+      comment: element.mail_forward_comment,
+      pending: element.in_progress
+    })
+  })
+  return json
+}
 
 function client () {
   return new kas.KasMailForward(process.env.KAS_USERNAME, process.env.KAS_PASSWORD)
