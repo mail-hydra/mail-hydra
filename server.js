c0fe6523a63c1d5e0a6ec01c55374c603dc369e1
Christopher <Christopher.Fenner@me.com>
2020-10-07 19:24:34 +0200

    fix standardJS findings

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const app = express()
 const kas = require('kas-api/dist/kas-mail-forward')
-
-function client(){
-    return new kas.KasMailForward(process.env.KAS_USERNAME, process.env.KAS_PASSWORD)
-}
+// Listen to the App Engine-specified port, or 8080 otherwise
+const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
-    client().list().then(function(results){
-        json = []
-        results.forEach(element => {
-            json.push({
-                source: element.mail_forward_adress,
-                target: element.mail_forward_targets,
-                comment: element.mail_forward_comment,
-                pending: element.in_progress,
-            })
-        });
-        res.json(json)
-    });
-});
+  client().list().then(function (results) {
+    var json = []
+    results.forEach(element => {
+      json.push({
+        source: element.mail_forward_adress,
+        target: element.mail_forward_targets,
+        comment: element.mail_forward_comment,
+        pending: element.in_progress
+      })
+    })
+    res.json(json)
+  })
+})
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`)
+})
 
-// Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
-});
+function client () {
+  return new kas.KasMailForward(process.env.KAS_USERNAME, process.env.KAS_PASSWORD)
+}
