b81967f2f3a286955c30606fa0da428856ee4852
Christopher <Christopher.Fenner@me.com>
2020-10-09 21:05:28 +0200

    use express and sequelize

--- server.js
+++ server.js
@@ -1,34 +1,30 @@
 require('dotenv').config()
-const express = require('express')
-const app = express()
-const kas = require('kas-api/dist/kas-mail-forward')
-// Listen to the App Engine-specified port, or 8080 otherwise
-const port = process.env.PORT || 8080
 
-app.get('/', (req, res) => {
-  client().list().then(function (results) {
-    res.json(forwardListToJSON(results))
-  })
-})
+const port = process.env.PORT || 3000
 
-// start app
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}...`)
-})
+const app = require('./express')
+const db = require('./sequelize')
 
-function forwardListToJSON (forwardList) {
-  var json = []
-  forwardList.forEach(element => {
-    json.push({
-      source: element.mail_forward_adress,
-      target: element.mail_forward_targets,
-      comment: element.mail_forward_comment,
-      pending: element.in_progress
-    })
-  })
-  return json
+async function assertDatabaseConnectionOk () {
+  console.log('Checking database connection...')
+  try {
+    await db.authenticate()
+    console.log('Database connection OK!')
+  } catch (error) {
+    console.log('Unable to connect to the database:')
+    console.log(error.message)
+    process.exit(1)
+  }
 }
 
-function client () {
-  return new kas.KasMailForward(process.env.KAS_USERNAME, process.env.KAS_PASSWORD)
+async function init () {
+  await assertDatabaseConnectionOk()
+
+  console.log(`Starting on port ${port}...`)
+
+  app.listen(port, () => {
+    console.log(`Express server started on port ${port}.`)
+  })
 }
+
+init()
